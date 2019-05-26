8e0d2e21cd51492b75867d2d1921541158c922f1
tsubasa1122 <nakanot@open8.com>
2019-05-26 20:29:17 +0900

    messageの修正

--- app.rb
+++ app.rb
@@ -17,9 +17,9 @@ end
 
 post '/callback' do
   hash = JSON.parse(request.body.read)
-  message = hash["entry"][0]["messaging"][0]
-  sender = message["sender"]["id"]
-  text = message["message"]["text"]
+  message = hash["entry"][0]["messaging"]
+  sender = message[0]["sender"]["id"]
+  text = message[4]["message"]["text"]
   endpoint = "https://graph.facebook.com/v3.3/me/messages?access_token=" + ENV["FACEBOOK_ACCESS_TOKEN_KEY"]
   content = {
     recipient: {id: sender},
