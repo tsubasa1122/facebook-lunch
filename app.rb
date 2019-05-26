ea9258771d0dc20f4206dea34f8d6bcae75730b4
tsubasa1122 <nakanot@open8.com>
2019-05-26 19:16:16 +0900

    callback urlの設定

--- app.rb
+++ app.rb
@@ -4,3 +4,11 @@ require 'sinatra/reloader'
 get '/' do
   "hello worsss"
 end
+
+get '/callback' do
+  if params["hub.verify_token"] != "hogehoge"
+    return "Error, wrong validation token"
+  end
+  params["hub.challenge"]
+end
+
